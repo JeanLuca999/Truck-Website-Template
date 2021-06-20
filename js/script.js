e43065e7bf25d633fa8a0688b343a363e1c3d1c6
JeanLuca999 <66375859+JeanLuca999@users.noreply.github.com>
2021-06-20 14:12:38 -0300

    finishing truck gallery and footer section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 const menu = document.querySelector('.header--menu')
 const navMenu = document.querySelector('#navMenu')
+const closeButton = document.querySelector('#close-button')
+
 menu.addEventListener('click', () => {
+    navMenu.classList.toggle('menu-active')
+})
+
+closeButton.addEventListener('click', () => {
     navMenu.classList.toggle('menu-active')
 })
